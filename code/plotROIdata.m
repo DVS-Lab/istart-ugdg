cc35bb25c0a5a4c3c7547b083f66e3cafe589a9d
Daniel Sazhin <tul03789@cla18994.tu.temple.edu>
2023-06-22 11:39:37 -0400

    up to date

--- code/plotROIdata.m
+++ code/plotROIdata.m
@@ -35,9 +35,8 @@ ID_Measure_1 = STRATEGIC.Proportion;  %STRATEGIC.Raw Proportion;
 ID_Measure_1_name= ' Proportion';
 ID_Measure_2 = COMPOSITE.StrategicXReward; %STRATEGIC.Proportion; %
 ID_Measure_2_name=' Int';%' Composite_SubstanceXReward_Squared'
-rois= {'pTPJ_extracted' 'seed-insula-bin'}; % 'pTPJ_extracted' 'seed-NAcc-thr' 'seed-vmPFC-5mm-thr'};% 'seed-pTPJ-bin' 'seed-mPFC-thr' 'seed-SPL-thr' 'seed-ACC-50-thr' 'seed-insula-thr'  'seed-dlPFC-thr'}; % 'seed-pTPJ-thr' 'seed-vmPFC-5mm-thr' 'seed-SPL-thr' 'seed-ACC-50-thr'}; % 'seed-dlPFC-UGR-bin' 'seed-ACC-10mm' 
-models = {['_type-ppi_seed-IFG_extracted_cov-COMPOSITE_noINT_model-GLM3_']}; % ppi_seed-IFG_extracted 'nppi-ecn' nppi-ecn ppi_seed-NAcc-bin act ppi_seed-IFG_extracted};
-
+rois= {'IFG_extracted' 'Insula_extracted'}; % 'pTPJ_extracted' 'seed-NAcc-thr' 'seed-vmPFC-5mm-thr'};% 'seed-pTPJ-bin' 'seed-mPFC-thr' 'seed-SPL-thr' 'seed-ACC-50-thr' 'seed-insula-thr'  'seed-dlPFC-thr'}; % 'seed-pTPJ-thr' 'seed-vmPFC-5mm-thr' 'seed-SPL-thr' 'seed-ACC-50-thr'}; % 'seed-dlPFC-UGR-bin' 'seed-ACC-10mm' 
+models = {['_type-act_cov-COMPOSITE_noINT_model-GLM3_']}; % ppi_seed-IFG_extracted 'nppi-ecn' nppi-ecn ppi_seed-NAcc-bin act ppi_seed-IFG_extracted};
 
 % 
 
@@ -50,15 +49,15 @@ models = {['_type-ppi_seed-IFG_extracted_cov-COMPOSITE_noINT_model-GLM3_']}; % p
 % Test hypotheses:
 
 H2 = 0; % Modulated and unmodulated cue activation. 
-H3 = 0; % Modulated and unmodulated choice activation. 
-H4 = 1; % Modulated and unmodulated NaCC PPI. 
+H3 = 1; % Modulated and unmodulated choice activation. 
+H4 = 0; % Modulated and unmodulated NaCC PPI. 
 H4_plot = 0; % Use if plotting multiple ROIs on the same bar plot. Code is crude and can only handle two ROIs.
 H5 = 0; % Modulated and unmodulated analysis of ECN.
 E1 = 0; % Modulated exploratory results for UGR (Ishika analysis)
 
 usenewroidir = 1; % Use new dir.
-modulated = 1;
-unmodulated = 0;
+modulated = 0;
+unmodulated = 1;
 
 %% Specify COPES and L3 models:
 
